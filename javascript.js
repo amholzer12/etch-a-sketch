a3d9eb8c0a13b81dd2d6be3110465f2be8b747d8
Austin Holzer <amholzer12@gmail.com>
2022-10-25 17:43:35 -0700

    create a function to build a board and implement the :hover affect

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-console.log("testing js")
+const canvas = document.querySelector(".canvas")
+// hard coding board size for building/testing purposes. will eventually be user input.
+const size = 16
+
+function buildBoard(size) {
+    const gridSize = size * size
+
+    for (let i = 0; i < gridSize; i++) {
+        //making div element to fill the canvas
+        const canvasDiv = document.createElement('div');
+        canvasDiv.classList.add("box");
+        canvasDiv.style.height = `${500/size}px`;
+        canvasDiv.style.width = `${500/size}px`;
+        canvasDiv.addEventListener('mouseover', (e) => {
+            canvasDiv.style.backgroundColor = 'black'
+        })
+
+        canvas.appendChild(canvasDiv);
+
+    }
+
+
+
+};
+
+buildBoard(size)
+console.log(canvas)
