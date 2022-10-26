68d987d87ebcdcf975004810948db82ce7a2abce
Austin Holzer <amholzer12@gmail.com>
2022-10-26 14:26:26 -0700

    only draw when mouse is pressed down AND moving

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector(".canvas")
 // hard coding board size for building/testing purposes. will eventually be user input.
 const size = 16
+let isDragging = false
 
 function buildBoard(size) {
     const gridSize = size * size
@@ -12,9 +13,18 @@ function buildBoard(size) {
         //take the size of the board (500px) and divide by how many squares you want on each side
         canvasDiv.style.height = `${500/size}px`;
         canvasDiv.style.width = `${500/size}px`;
+        //only want to draw when mouse is clicked down AND moving
+        canvasDiv.addEventListener('mousedown', (e) => {
+            isDragging = true;
+        });
+        canvasDiv.addEventListener('mouseup', (e) => {
+            isDragging = false;
+        });
         canvasDiv.addEventListener('mouseover', (e) => {
-            canvasDiv.style.backgroundColor = 'black'
-        })
+            if (isDragging == true) {
+                canvasDiv.style.backgroundColor = 'black';
+            }
+        });
 
         canvas.appendChild(canvasDiv);
 
